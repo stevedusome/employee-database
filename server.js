2883ac03e74442c1744ee76f69856ec5edc51d66
Steve Dusome <stevedusome@gmail.com>
2022-02-13 22:01:46 -0500

    finished all functions except update

--- server.js
+++ server.js
@@ -35,19 +35,10 @@ const db = mysql.createConnection(
   console.log('Connected to the employee database.')
 );
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
-
-app.use((req, res) => {
-    res.status(404).end();
-  });
-
 displayDepartments = function () {
   db.query(`SELECT * FROM departments`, (err, rows) => {
-    console.table(rows);
+    console.table("\n",rows,"\n");
+    return true;
   });
 };
 
@@ -60,7 +51,7 @@ displayRoles = function () {
   if (err) {
   console.log(err);
   }
-console.table(result);
+return console.table("\n",result,"\n");
 });
 };
 
@@ -73,24 +64,231 @@ displayEmployees = function () {
   if (err) {
   console.log(err);
   }
-console.table(result);
+return console.table("\n",result,"\n");
 });
 };
 
-mainMenu = function () {
+addDepartment = async function () {
+  const questions = [
+    {
+      type: 'text',
+      name: 'departmentName',
+      message: "What is the name of the new Department?",
+      validate: departmentNameInput => {
+          if (departmentNameInput) {
+              return true;
+          }
+          else {
+              console.log("Please enter name of your department");
+              return false;
+          }}
+    } ];
+
+    return inquirer.prompt(questions)
+    .then((answers) => {
+      const name = answers.departmentName;
+      const sql = `INSERT INTO departments (department_name) 
+              VALUES (?)`;
+
+              db.query(sql, name, (err, result) => {
+                if (err) {
+                  console.log(err);
+                }
+                return console.log("Department saved");
+              });
+})
+};
+
+addRole = async function () {
+  
+  let currentDepartments = []
+  db.query(`SELECT * FROM departments`, (err, rows) => {
+    
+    for (let i = 0; i < rows.length; i++) {
+    let thisDepartment = `${rows[i].id}. ${rows[i].department_name}`;
+    currentDepartments.push(thisDepartment);
+    };
+
+  });
+
+  const questions = [
+    {
+      type: 'text',
+      name: 'roleTitle',
+      message: "What is the title of the new Role?",
+      validate: roleTitleInput => {
+          if (roleTitleInput) {
+              return true;
+          }
+          else {
+              console.log("Please enter the title of the role");
+              return false;
+          }}
+    },
+    {
+      type: 'number',
+      name: 'roleSalary',
+      message: "What is the salary of the new Role?",
+      validate: roleSalaryInput => {
+          if (roleSalaryInput) {
+              return true;
+          }
+          else {
+              console.log("Please enter salary of the role");
+              return false;
+          }}
+    },
+    {
+      type: 'list',
+      name: 'roleDepartment',
+      message: 'Which department does the role belong to?',
+      choices: currentDepartments
+     }
+  ];    
+
+  return inquirer.prompt(questions)
+    .then((answers) => {
+      const departmentNumber = answers.roleDepartment.match(/(\d+)/);;
+      const params = [answers.roleTitle, answers.roleSalary, departmentNumber[0]];
+      const sql = `INSERT INTO roles (title, salary, department_id) 
+              VALUES (?,?,?)`;
+
+              db.query(sql, params, (err, result) => {
+                if (err) {
+                  console.log(err);
+                }
+                return console.log("Role saved");
+              });
+})
+};
+
+addEmployee = async function () {
+  let currentRoles = []
+  let currentEmployees = []
+
+  db.query(`SELECT * FROM employees`, (err, rows) => {
+    
+    for (let i = 0; i < rows.length; i++) {
+    let thisEmployee = `${rows[i].id}. ${rows[i].first_name} ${rows[i].last_name}`;
+    currentEmployees.push(thisEmployee);
+    };
+    currentEmployees.push('None')
+
+  });
+
+  db.query(`SELECT * FROM roles`, (err, rows) => {
+    
+    for (let i = 0; i < rows.length; i++) {
+    let thisRole = `${rows[i].id}. ${rows[i].title}`;
+    currentRoles.push(thisRole);
+    };
+
+  });
+
+  const questions = [
+    {
+      type: 'text',
+      name: 'firstName',
+      message: "What is the employees first name?",
+      validate: firstNameInput => {
+          if (firstNameInput) {
+              return true;
+          }
+          else {
+              console.log("Please enter the first name");
+              return false;
+          }}
+    },
+    {
+      type: 'text',
+      name: 'lastName',
+      message: "What is the employees last name?",
+      validate: lastNameInput => {
+          if (lastNameInput) {
+              return true;
+          }
+          else {
+              console.log("Please enter the last name");
+              return false;
+          }}
+    },
+    {
+      type: 'list',
+      name: 'employeeRole',
+      message: 'What is the employees role?',
+      choices: currentRoles
+     },
+     {
+      type: 'list',
+      name: 'employeeManager',
+      message: 'Who is the employees manager?',
+      choices: currentEmployees
+     }
+  ];    
+
+  return inquirer.prompt(questions)
+    .then((answers) => {
+      let managerNumber
+      if (answers.employeeManager === 'None') {
+        managerNumber = NULL;
+      } else {
+        managerNumber = answers.employeeManager.match(/(\d+)/);;
+      }
+      
+      const roleNumber = answers.employeeRole.match(/(\d+)/);;
+      const params = [answers.firstName, answers.lastName, roleNumber[0], managerNumber[0]];
+      const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+              VALUES (?,?,?,?)`;
+
+              db.query(sql, params, (err, result) => {
+                if (err) {
+                  console.log(err);
+                }
+                return console.log("Employee saved");
+              });
+})
+};
+
+mainMenu = async function () {
   return inquirer.prompt(mainMenuQuestion)
   .then ((answers) => {
+
     if (answers.startQuestion === 'View Departments') {
-      displayDepartments();
+
+        return displayDepartments();
+
     } else if (answers.startQuestion === 'View Roles') {
-      displayRoles();
+
+      return displayRoles();
+
     } else if (answers.startQuestion === 'View Employees') {
-      displayEmployees();
+
+      return displayEmployees();
+
+    } else if (answers.startQuestion === 'Add a Department') {
+
+      return addDepartment();
+
+    } else if (answers.startQuestion === 'Add a Role') {
+
+      return addRole();
+
+    } else if (answers.startQuestion === 'Add an Employee') {
+
+      return addEmployee();
+
     } else {
+
       greeting = "Good evening";
     }
   console.log(answers.startQuestion);
 })
+};
+
+mainMenuLoop = async function() {
+  while (true === true) {
+  await mainMenu();;
+}
 };
 
 startUp = async function()
@@ -100,7 +298,8 @@ await app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   })
   console.log('this is the Main Menu');
-  mainMenu();
+
+  return mainMenuLoop();
 }
 
 startUp();
